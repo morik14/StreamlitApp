3b0ecdbe37b56131d7d742d65fb657c43345d5ae
Bepolin <morikazu112233@gmail.com>
2024-05-20 00:17:13 +0900

    update service

--- src/app/services/home_page_service.py
+++ src/app/services/home_page_service.py
@@ -1,15 +1,13 @@
 from domain.entities import MeasureEntity
-from domain.repositories import MeasureRepository, IMeasureRepository
+from domain.services import MeasureService
 
 
 class HomePageService:
 
-    _measure_repository: IMeasureRepository = MeasureRepository()
-
     @classmethod
     def save_measure(cls, area_id: int, measure_value: float):
         measure = MeasureEntity(area_id=area_id, measure_value=measure_value)
-        cls._measure_repository.save(measure=measure)
+        MeasureService.calc(measure=measure)
 
     @staticmethod
     def get_latest():
--- src/domain/services/measure_service.py
+++ src/domain/services/measure_service.py
@@ -1,7 +1,13 @@
+from domain.entities import MeasureEntity
+from domain.repositories import MeasureRepository, IMeasureRepository
+
+
 class MeasureService:
+
+    _model_repository: IMeasureRepository = MeasureRepository()
 
     def __init__(self) -> None:
         pass
 
-    def calc(self):
+    def calc(self, measure: MeasureEntity):
         pass
